2c76b5932aeee1205bc8a4d6908529724a72010c
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-13 19:31:39 -0300

    connect to mongodb atlas

---
+++ src/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const uri =
+  `mongodb+srv://${process.env.MONGODB_CREDENTIALS}@cluster0-uvim4.gcp.mongodb.net/test?retryWrites=true&w=majority";`
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDb database is up and running");
+});
