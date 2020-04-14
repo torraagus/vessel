bc32e7fbd377812d2950cbf06f1d487227dbd741
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-13 21:38:52 -0300

    add middlewares and routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // settings
 app.set("port", process.env.PORT || 4000);
 
-// middleware
+// middleware - Se ejecutan antes de llegar a las urls
+app.use(cors());
+app.use(express.json());
 
 // routes
+app.use('/api/users', require('./routes/users.routes'));
 
 module.exports = app; 
