527d4e82f50611b5c253bd911e8840ec1da0bc48
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-15 18:13:00 -0300

    add login route and implement jsw

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+const {ensureToken, verifyToken} = require('./middlewares/jwt');
 
 // settings
 app.set("port", process.env.PORT || 4000);
@@ -10,6 +13,13 @@ app.use(cors());
 app.use(express.json());
 
 // routes
-app.use('/api/users', require('./routes/users.routes'));
+app.use('/api/users', ensureToken, verifyToken, require('./routes/users.routes'));
+app.post('/api/login', (req, res) => {
+    const user = {email: 'agu@gmail.com', password: 'password'};
+    const token = jwt.sign({user}, process.env.JWT_SECRET_KEY);
+    res.json({
+        token
+    });
+});
 
 module.exports = app; 
