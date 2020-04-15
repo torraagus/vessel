91bc9ba7c055397086035a62b6360ee286643074
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-15 18:16:33 -0300

    create functions to ensure and validate token

---
+++ src/middlewares/jwt.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+function ensureToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (bearerHeader) {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+};
+
+function verifyToken(req, res, next) {
+    jwt.verify(req.token, process.env.JWT_SECRET_KEY, (err, data) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            next();
+        }
+    })
+};
+
+module.exports = {ensureToken, verifyToken};
