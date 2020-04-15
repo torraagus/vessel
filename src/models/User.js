711aabd5884da2d752fc33555f8530213b2b3ed5
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-14 22:00:19 -0300

    create user schema with validations

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+    },
+    email: {
+      type: String,
+      validate: {
+        validator: function(v) {
+          return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(v);
+        },
+        message: props => `${props.value} is not a valid email address`
+      },
+    },
+    password: {
+      type: String,
+      minlength: [8, 'Password is too short'],
+    },
+    fullname: {
+      type: String,
+      required: [true, 'Fullname is required'],
+      trim: true,
+      set: (str) => str.replace(/\s\s+/g, " "),
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", userSchema);
