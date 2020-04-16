0aa5d1f3241a57bfda9b783e5efa108ff7d4e713
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-15 22:07:43 -0300

    refactor login route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-const jwt = require('jsonwebtoken');
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-const {ensureToken, verifyToken} = require('./middlewares/jwt');
+const { ensureToken, verifyToken } = require("./middlewares/jwt");
 
 // settings
 app.set("port", process.env.PORT || 4000);
@@ -13,13 +12,12 @@ app.use(cors());
 app.use(express.json());
 
 // routes
-app.use('/api/users', ensureToken, verifyToken, require('./routes/users.routes'));
-app.post('/api/login', (req, res) => {
-    const user = {email: 'agu@gmail.com', password: 'password'};
-    const token = jwt.sign({user}, process.env.JWT_SECRET_KEY);
-    res.json({
-        token
-    });
-});
+app.use(
+  "/api/users",
+  ensureToken,
+  verifyToken,
+  require("./routes/users.routes")
+);
+app.use("/api/login", require("./routes/login.routes"));
 
-module.exports = app; 
+module.exports = app;
