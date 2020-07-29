6379521991e9cb8a9a2e670ba36272668680ffe5
torraagus <torra.agustinignacio@gmail.com>
2020-07-29 14:59:51 -0300

    update login api controller

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,21 +1,21 @@
 const loginCtrl = {};
 
 const jwt = require("jsonwebtoken");
-const Login = require("../models/Login");
+const User = require("../models/User");
 
-loginCtrl.loginUser = (req, res) => {
-  const user = new Login(req.body);
-  error = user.validateSync();
-  if (error) {
-    console.log(`${error}`);
-    res.json({
-      message: `${error}`,
-    });
-  } else {
-    // The token expires in 30 minutes  
+loginCtrl.loginUser = async (req, res) => {
+  console.log(req.body);
+  const user = await User.find(req.body);
+  if (user.length > 0) {
+    console.log(user);
     const token = jwt.sign({ login: user }, process.env.JWT_SECRET_KEY, { expiresIn: '30m' });
     res.json({
       token,
+    });
+  } else {
+    console.log(`Wrong credentials`);
+    res.json({
+      message: `Wrong credentials`,
     });
   }
 };
